13a5a004a4ffbccd29070535ce6e0ced4a6dc34c
Andre Baltieri <andrebaltieri@hotmail.com>
2016-05-18 06:14:34 -0300

    Adicionado ícones e suporte ao electron

---
+++ electron.js
@@ -0,0 +1,29 @@
+// electron-packager ./ 2doo --platform=win32 --arch=x64 --version=1.1.0 --out=./build --overwrite=true --asar=true --icon=C:\dev\2doo\wwwroot\assets\img\2doo.ico
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+    mainWindow.loadURL(`file://${__dirname}/wwwroot/index.html`)
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
